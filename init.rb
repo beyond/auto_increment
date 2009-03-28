4e5caaf0385f8366a6d12c09b2014f7358677f5c
Beyond <a902_beyond@yahoo.co.jp>
2009-03-29 06:42:04 +0900

    add

---
+++ init.rb
@@ -0,0 +1,20 @@
+# Include hook code here
+class ActiveRecord::ConnectionAdapters::SQLiteAdapter
+	def set_auto_increment( table_name, value )
+		execute( "update sqlite_sequence set seq=#{value} where name='#{table_name}';" )
+	end
+end
+
+class ActiveRecord::ConnectionAdapters::MysqlAdapter
+	def set_auto_increment( table_name, value )
+		execute( "alter table #{table_name} auto_increment=#{value};" )
+	end
+end
+
+class ActiveRecord::Base
+	def self.auto_increment=(value)
+		value = value.to_i
+		return if value <= 0
+		connection.set_auto_increment( quoted_table_name, value )
+	end
+end
---
+++ tasks/auto_increment_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :auto_increment do
+#   # Task goes here
+# end
